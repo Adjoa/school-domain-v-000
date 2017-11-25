1c4ed44c7a2416599c4f2d6225fdba2edb3a8f9f
Adjoa Darien <adjoa.darien@gmail.com>
2017-11-25 22:58:07 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -2,16 +2,20 @@ class School
   attr_accessor :roster
   attr_reader :name
 
-
-
   def initialize(name)
     @name = name
     @roster = {}
   end
 
   def add_student(student, grade)
-    @roster["#{student}"] = []
-    @roster["#{student}"] << grade
+    @roster[grade] = []
+    @roster[grade] << student
+  end
+
+  def grade
+  end
+
+  def sort
   end
 
 end
