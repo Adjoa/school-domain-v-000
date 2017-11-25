e6c919f9cf536c943fbde1dea883689edd2ad870
Adjoa Darien <adjoa.darien@gmail.com>
2017-11-25 22:59:32 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -8,7 +8,10 @@ class School
   end
 
   def add_student(student, grade)
-    @roster[grade] = []
+    unless @roster.include?(grade) do
+      @roster[grade] = []
+    end
+    
     @roster[grade] << student
   end
 
