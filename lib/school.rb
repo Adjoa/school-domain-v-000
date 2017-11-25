13ad8b48f42cd7272fb07f24693828d046e916ec
Adjoa Darien <adjoa.darien@gmail.com>
2017-11-25 23:19:59 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -19,10 +19,10 @@ class School
     @roster[grade]
   end
 
-  def sort
-    @roster.each do
-      @roster[grade].sort!
-    end
-  end
+  # def sort
+  #   @roster.each do
+  #     @roster[grade].sort!
+  #   end
+  # end
 
 end
